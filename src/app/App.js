35afa6e6e1627d6ebe582ab0ee60df53d9553333
yg-0103 <rladusrn89@gmail.com>
2021-04-07 23:08:29 +0900

    [Feat] Navbar 컴포넌트

--- src/app/App.js
+++ src/app/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Routes from '../routes';
 import { BrowserRouter } from 'react-router-dom';
 import Logo from '../components/Logo/Logo';
+import Navbar from '@components/Navbar/Navbar';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Logo tag="h2" />
+      <Navbar />
       <Routes />
     </BrowserRouter>
   );
