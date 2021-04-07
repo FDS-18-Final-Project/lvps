2c3540318b9b6117b8a1d27790d9c8cddecde126
dannylee9212 <dannylee9212@gmail.com>
2021-04-07 12:09:47 +0900

    [Conf] develop branch 만듬

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import appReducers from './store';
 
 //web accessbility
 import { autoA11yTest } from './utils/autoA11yTest';
+
 // 한국어 출력
 autoA11yTest();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
