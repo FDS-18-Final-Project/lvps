d4ea2217d54225079a0a01027f33aa9345ffc85a
GEUNA <geuna0204@gmail.com>
2021-04-10 15:03:01 +0900

    [Refactor] MainBanner 컨테이너에 Divider 컴포넌트 적용

--- src/components/Divider/Divider.stories.js
+++ src/components/Divider/Divider.stories.js
@@ -1,34 +1,34 @@
-import { Divider } from "components";
-import { ThemeProvider } from "styled-components";
-import theme from "theme/theme";
-import GlobalStyle from "styles/GlobalStyle";
+import { Divider } from 'components';
+import { ThemeProvider } from 'styled-components';
+import theme from 'theme/theme';
+import GlobalStyle from 'styles/GlobalStyle';
 
 //스토리 구성 객체
 export default {
-  title: " MMAuto/Component/Divider",
+  title: ' MMAuto/Component/Divider',
   components: Divider,
   parameters: {
     docs: {
       description: {
-        component: "구분선으로 사용할 수 있는 Divider 컴포넌트입니다.",
-      },
+        component: '구분선으로 사용할 수 있는 Divider 컴포넌트입니다.'
+      }
     },
     design: {
-      type: "figma",
+      type: 'figma',
       url:
-        "https://www.figma.com/file/UqapsvM6RAF371unsQkgus/%EC%B0%A8%EB%9F%89%EC%84%9C%EB%B9%84%EC%8A%A4(%EC%88%98%EC%A0%95)?node-id=3%3A8",
-    },
+        'https://www.figma.com/file/UqapsvM6RAF371unsQkgus/%EC%B0%A8%EB%9F%89%EC%84%9C%EB%B9%84%EC%8A%A4(%EC%88%98%EC%A0%95)?node-id=3%3A8'
+    }
   },
   argTypes: {
-    width: { type: "text" },
-    height: { type: "text" },
-    bgColor: { type: "text" },
-    marginTop: { type: "text" },
-    marginBottom: { type: "text" },
-  },
+    width: { type: 'text' },
+    height: { type: 'text' },
+    bgColor: { type: 'text' },
+    marginTop: { type: 'text' },
+    marginBottom: { type: 'text' }
+  }
 };
 
-const Template = (args) => (
+const Template = (...args) => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
     <Divider {...args}></Divider>
@@ -39,9 +39,9 @@ const Template = (args) => (
 export const NormalDivider = Template.bind({});
 
 NormalDivider.args = {
-  width: "110px",
-  height: "4px",
-  bgColor: "lightGray",
-  marginTop: "45px",
-  marginBottom: "45px",
+  width: '110px',
+  height: '4px',
+  bgColor: 'lightGray',
+  marginTop: '45px',
+  marginBottom: '45px'
 };
