c60d6cffb5f42e57ae40d76f09f1d70ddb422eff
yg-0103 <rladusrn89@gmail.com>
2021-04-15 10:11:08 +0900

    [Feat] ServiceInfo 컨테이너 반응형 추가

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Routes from '../routes';
 import { BrowserRouter } from 'react-router-dom';
-import { Header, Footer, SubBanner, OurBestServices } from 'containers';
-import PriceTable from '../components/PriceTable/PriceTable';
+import {
+  Header,
+  Footer,
+  SubBanner,
+  OurBestServices,
+  ServiceInfo
+} from 'containers';
 
 const App = () => {
   return (
@@ -10,7 +15,13 @@ const App = () => {
       <Header />
       <Routes />
       <SubBanner />
-      <PriceTable />
+      <ServiceInfo>
+        Ceramic Pro is a multi-layerable, clear, liquid nano-ceramic coating.
+        When cured, this technology will transform itself on the surface to a
+        permanent, durable yet flexible glass shield. Ceramic Pro can be
+        described as an additional clear coat, with 3 times the hardness and
+        self cleaning properties.
+      </ServiceInfo>
       <OurBestServices />
       <Footer>
         <Footer.Logo />
