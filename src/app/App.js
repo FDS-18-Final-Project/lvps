401be9fea6e56267e948e63eed747fe56ce4a754
yg-0103 <rladusrn89@gmail.com>
2021-04-17 13:08:10 +0900

    [Feat] FAQ 컨테이너

--- src/app/App.js
+++ src/app/App.js
@@ -2,16 +2,13 @@ import React from 'react';
 import Routes from '../routes';
 import { BrowserRouter } from 'react-router-dom';
 import { Header, Footer, FAQ, OurBestServices } from 'containers';
-import GetAquoteCheckbox from '../containers/GetAQuoteCheckbox/GetAQuoteCheckbox';
-import StaffIntroduction from 'containers/StaffIntroduction/StaffIntroduction';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <FAQ />
-      <OurBestServices />
       <Routes />
+      <FAQ />
       <Footer>
         <Footer.Logo />
         <Footer.Content />
