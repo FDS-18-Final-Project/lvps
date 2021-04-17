fe7075eacf3519c35b7ed4e422ce689f164c03c3
jiwon <iamkjw77@gmail.com>
2021-04-17 17:37:19 +0900

    [Feat] 가격표 중간완성

--- src/containers/CeramicPrice/CeramicPrice.js
+++ src/containers/CeramicPrice/CeramicPrice.js
@@ -1,28 +1,93 @@
 import { PriceCardLayout } from 'components/';
 
-const infoList = [
-  [
-    {
-      id: 1,
-      title: '4 Layers Ceramic Pro 9H',
-      description: 'on all painted surfaces and trim'
-    },
-    {
-      id: 2,
-      title: '4 Layers Ceramic Pro 9H',
-      description: 'on all painted surfaces and trim'
-    },
-    {
-      id: 3,
-      title: '4 Layers Ceramic Pro 9H',
-      description: 'on all painted surfaces and trim'
-    },
-    {
-      id: 4,
-      title: '4 Layers Ceramic Pro 9H',
-      description: 'on all painted surfaces and trim'
-    }
-  ]
+const contents = [
+  {
+    id: 1,
+    tagType: 'tagGold',
+    tagText: 'GOLD',
+    price: '1499',
+
+    cardInfo: [
+      {
+        id: 1,
+        title: '4 Layers Ceramic Pro 9H',
+        description: 'on all painted surfaces and trim'
+      },
+      {
+        id: 2,
+        title: '4 Layers Ceramic Pro 9H',
+        description: 'on all painted surfaces and trim'
+      },
+      {
+        id: 3,
+        title: '4 Layers Ceramic Pro 9H',
+        description: 'on all painted surfaces and trim'
+      },
+      {
+        id: 4,
+        title: '4 Layers Ceramic Pro 9H',
+        description: 'on all painted surfaces and trim'
+      }
+    ]
+  },
+  {
+    id: 2,
+    tagType: 'tagSilver',
+    tagText: 'SILVER',
+    price: '400',
+
+    cardInfo: [
+      {
+        id: 1,
+        title: '4 Layers Ceramic Pro 9H',
+        description: 'on all painted surfaces and trim'
+      },
+      {
+        id: 2,
+        title: '4 Layers Ceramic Pro 9H',
+        description: 'on all painted surfaces and trim'
+      },
+      {
+        id: 3,
+        title: '4 Layers Ceramic Pro 9H',
+        description: 'on all painted surfaces and trim'
+      },
+      {
+        id: 4,
+        title: '4 Layers Ceramic Pro 9H',
+        description: 'on all painted surfaces and trim'
+      }
+    ]
+  },
+  {
+    id: 3,
+    tagType: 'tagBronze',
+    tagText: 'BRONZE',
+    price: '400',
+
+    cardInfo: [
+      {
+        id: 1,
+        title: '4 Layers Ceramic Pro 9H',
+        description: 'on all painted surfaces and trim'
+      },
+      {
+        id: 2,
+        title: '4 Layers Ceramic Pro 9H',
+        description: 'on all painted surfaces and trim'
+      },
+      {
+        id: 3,
+        title: '4 Layers Ceramic Pro 9H',
+        description: 'on all painted surfaces and trim'
+      },
+      {
+        id: 4,
+        title: '4 Layers Ceramic Pro 9H',
+        description: 'on all painted surfaces and trim'
+      }
+    ]
+  }
 ];
 
 const CeramicPrice = () => {
@@ -31,7 +96,7 @@ const CeramicPrice = () => {
       title="Ceramic Pro Automotive Packages"
       type="title"
       icon={false}
-      infoList={infoList}
+      contents={contents}
     />
   );
 };
