03fd4d4448ab3ba48c08e661e73b9eaf35f9c458
dannylee9212 <dannylee9212@gmail.com>
2021-04-20 20:37:42 +0900

    [Feat] ContactUs 페이지 완성 및 submainbanner 뒷배경 dimmed효과

--- src/app/App.js
+++ src/app/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Routes from '../routes';
 import { BrowserRouter } from 'react-router-dom';
 import Header from 'containers/Header/Header';
-import MainServiceCarousel from 'containers/MainServiceCarousel/MainServiceCarousel';
 import Footer from 'containers/Footer/Footer';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <MainServiceCarousel />
       <Routes />
       <Footer />
     </BrowserRouter>
