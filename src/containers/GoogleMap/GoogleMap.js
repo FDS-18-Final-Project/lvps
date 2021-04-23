b6e0310435f3f4c9c278e0ce307161e27ec63b9c
dannylee9212 <dannylee9212@gmail.com>
2021-04-23 14:32:31 +0900

    [Feat] 구글맵 아이콘 추가

--- src/containers/GoogleMap/GoogleMap.js
+++ src/containers/GoogleMap/GoogleMap.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import Icon from 'components/Icon/Icon';
 
-require('dotenv').config();
-
-const AnyReactComponent = ({ text }) => (
+const AnyReactComponent = () => (
   <div
     style={{
-      width: '50px',
-      height: '20px',
-      borderRadius: '5px',
-      boxShadow: '0 1px 3px #FFBF00',
-      backgroundColor: '#FFBF00',
-      position: 'relative',
-      border: '5px solid #FFBF00',
-      fontSize: '18px',
-      textAlign: 'center'
+      width: '35px',
+      height: '40px',
+      position: 'relative'
     }}
   >
-    {text}
+    <Icon type="pin" />
   </div>
 );
 
@@ -27,8 +20,7 @@ class GoogleMap extends Component {
       lat: 43.75982215186903,
       lng: -79.47634137563207
     },
-    zoom: 15,
-    lang: 'en'
+    zoom: 15
   };
 
   render() {
@@ -39,11 +31,7 @@ class GoogleMap extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <AnyReactComponent
-            lat={43.75982215186903}
-            lng={-79.47634137563207}
-            text="Here!"
-          />
+          <AnyReactComponent lat={43.75982215186903} lng={-79.47634137563207} />
         </GoogleMapReact>
       </div>
     );
